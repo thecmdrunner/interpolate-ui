757289d5687d7b52df0d131386e263645b8da008
Pranav <hey@pranava.dev>
2024-05-26 16:50:09 +0530

    add fps and text controls

--- components/codeblock.tsx
+++ components/codeblock.tsx
@@ -1,7 +1,31 @@
 import fs from "fs";
+import WordsGradualSpacing from "./gradual-spacing";
 
 export default async function CodeBlock() {
-  const code = fs.readFileSync("./components/gradual-spacing.tsx", "utf-8");
+  const _code = fs.readFileSync("./components/gradual-spacing.tsx", "utf-8");
+  const codeToShow = _code.split("// COMPONENT_END")[0]!;
 
-  return <>{code}</>;
+  return (
+    <>
+      <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+        See it in action
+      </h4>
+      <WordsGradualSpacing />
+
+      <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+        Use this code
+      </h4>
+
+      <div className="whitespace-pre-wrap bg-slate-700 p-3.5 pb-3 text-sm rounded-t-lg border flex justify-between items-center border-slate-600 border-b-0 font-mono">
+        components/gradual-spacing.tsx
+        <div className="flex items-center gap-2">
+          <button>copy</button>
+          <button>download</button>
+        </div>
+      </div>
+      <div className="whitespace-pre-wrap bg-slate-800 p-3.5 pt-3 rounded-b-lg border border-slate-600 font-mono">
+        {codeToShow}
+      </div>
+    </>
+  );
 }
--- components/gradual-spacing.tsx
+++ components/gradual-spacing.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Player } from "@remotion/player";
-import React from "react";
+import React, { useState } from "react";
 import {
   AbsoluteFill,
   interpolate,
@@ -75,29 +75,62 @@ export const WordsGradualSpacing: React.FC<WordsGradualSpacingProps> = ({
     </AbsoluteFill>
   );
 };
+// COMPONENT_END
 
 export default () => {
+  const [sentence, setsentence] = useState(
+    "This text animates with gradual spacing!"
+  );
+
+  const [fps, setfps] = useState(30);
+
   return (
-    <div className="w-96 h-[216px] rounded-lg overflow-hidden border border-slate-400 bg-white">
-      <Player
-        loop
-        controls
-        allowFullscreen
-        autoPlay
-        clickToPlay
-        style={{
-          width: "100%",
-          height: "100%",
-        }}
-        inputProps={{
-          sentence: "This text animates with gradual spacing",
-        }}
-        component={WordsGradualSpacing}
-        durationInFrames={100}
-        compositionWidth={1920}
-        compositionHeight={1080}
-        fps={30}
-      />
+    <div className="flex flex-wrap justify-between gap-3">
+      <div className="w-full sm:w-96 aspect-video rounded-lg overflow-hidden border border-slate-400 bg-white">
+        <Player
+          loop
+          controls
+          allowFullscreen
+          autoPlay
+          clickToPlay
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+          inputProps={{
+            sentence,
+          }}
+          component={WordsGradualSpacing}
+          durationInFrames={3 * fps}
+          compositionWidth={1920}
+          compositionHeight={1080}
+          fps={fps}
+        />
+      </div>
+
+      <div className="flex flex-col w-full sm:w-max gap-3">
+        <div className="flex flex-col w-full">
+          Change text:
+          <input
+            type="text"
+            defaultValue={sentence}
+            onChange={(e) => setsentence(e.target.value)}
+            className="border h-14 p-2 w-full border-slate-400 rounded-lg"
+          />
+        </div>
+        <div className="flex flex-col">
+          FPS:
+          <select
+            value={fps}
+            onChange={(e) => setfps(parseInt(e.target.value))}
+            className="border w-20 h-14 p-2 pl-3 border-slate-400 rounded-lg"
+          >
+            <option value="30">30</option>
+            <option value="60">60</option>
+            <option value="120">120</option>
+          </select>
+        </div>
+      </div>
     </div>
   );
 };
